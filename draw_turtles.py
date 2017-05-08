282761474817b19b5a4c9814a9ae66a1056476c5
Kurt <kurtanderson234@gmail.com>
2017-05-08 12:55:43 -0500

    Updated to draw a circle of either triangles or squares.

--- draw_turtles.py
+++ draw_turtles.py
@@ -1,25 +1,35 @@
 import turtle
 
-def draw_square():
-    
+
+def main():
+    # Initialize the screen
     window = turtle.Screen()
     window.bgcolor("white")
-    
+
+    # Initialize the cursor
     cursor = turtle.Turtle()
     cursor.shape("turtle")
     cursor.color("blue")
-    
-    cursor.forward(100)
-    cursor.right(90)
-    cursor.forward(100)
-    cursor.right(90)
-    cursor.forward(100)
-    cursor.right(90)
-    cursor.forward(100)
-    cursor.right(90)
-    
+    cursor.speed(100)
 
+    # Draw a circle of the requested Shape
+    for i in range (0,45):
+        draw_triangle(cursor)
+        cursor.right(8)
     window.exitonclick()
+    
+def draw_square(mTurtle):
+    # Draws a Square
+    for x in range (0,4):
+        mTurtle.forward(100)
+        mTurtle.right(90)
+        
 
+def draw_triangle(mTurtle):
+    # Draws a triangle
+    for x in range (0,3):
+        mTurtle.forward(100)
+        mTurtle.right(120)
 
-draw_square()
+main()
+
